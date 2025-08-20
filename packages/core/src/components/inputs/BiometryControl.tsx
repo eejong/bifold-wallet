740fedb13d27976a877b8821db72cead8f3ddf08
vinjai <pumarasaravinjai@gmail.com>
2025-08-20 15:06:26 +0800

    biometry

--- packages/core/src/components/inputs/BiometryControl.tsx
+++ packages/core/src/components/inputs/BiometryControl.tsx
@@ -11,6 +11,7 @@ import { useAuth } from '../../contexts/auth'
 import { useTheme } from '../../contexts/theme'
 import { testIdWithKey } from '../../utils/testable'
 import { getSupportedBiometryType, BIOMETRY_TYPE } from 'react-native-keychain'
+import biometryImg from '../../assets/img/biometry.svg'
 
 const BIOMETRY_PERMISSION = PERMISSIONS.IOS.FACE_ID
 
@@ -149,7 +150,7 @@ const BiometryControl: React.FC<BiometryControlProps> = ({ biometryEnabled, onBi
       )}
       <ScrollView style={styles.container}>
         <View style={{ alignItems: 'center' }}>
-          <Assets.svg.biometrics style={styles.image} />
+          <biometryImg height={200} />
         </View>
         {biometryAvailable ? (
           <View style={styles.biometryAvailableRowGap}>
