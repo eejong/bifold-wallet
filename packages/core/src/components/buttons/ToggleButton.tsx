c0c3c4f3447f4db32088288d0728b52234163930
vinjai <pumarasaravinjai@gmail.com>
2025-08-27 14:07:05 +0800

    try

--- packages/core/src/components/buttons/ToggleButton.tsx
+++ packages/core/src/components/buttons/ToggleButton.tsx
@@ -37,7 +37,7 @@ const ToggleButton: React.FC<ToggleButtonProps> = ({
   // Animate thumb position
   const translateX = toggleAnim.interpolate({
     inputRange: [0, 1],
-    outputRange: [0, 18],
+    outputRange: [0, 10],
   })
 
   return (
@@ -56,10 +56,10 @@ const ToggleButton: React.FC<ToggleButtonProps> = ({
           height: 22,
           borderRadius: 11,
           borderWidth: 2,
-          borderColor: isEnabled
-            ? ColorPalette.brand.primary
-            : ColorPalette.grayscale.lightGrey,
-          backgroundColor: '#FFFFFF', // stays white like your screenshot
+          borderColor:  ColorPalette.brand.primary,            
+          backgroundColor: isEnabled
+          ? ColorPalette.brand.primary
+          : ColorPalette.grayscale.white, // stays white like your screenshot
           padding: 2,
           justifyContent: 'center',
           opacity: disabled ? 0.5 : 1,
@@ -68,15 +68,12 @@ const ToggleButton: React.FC<ToggleButtonProps> = ({
         <Animated.View
           style={{
             transform: [{ translateX }],
-            width: 18,
-            height: 18,
+            width: 10,
+            height: 10,
             borderRadius: 9,
             backgroundColor: isEnabled
               ? ColorPalette.brand.primary
               : ColorPalette.grayscale.lightGrey,
-            shadowColor: '#000',
-            shadowOpacity: 0.15,
-            shadowRadius: 2,
             elevation: 2,
           }}
         />
