3d7f3649b979adfc3377c8b4fe5ae641d01d3bbe
Arvin Jai Pumaras <arvinjai.pumaras@asti.dost.gov.ph>
2025-09-25 15:17:31 +0800

    hays

--- packages/core/src/screens/ImportWallet.tsx
+++ packages/core/src/screens/ImportWallet.tsx
@@ -5,15 +5,12 @@ import type { StackNavigationProp } from '@react-navigation/stack'
 import { OnboardingStackParams, Screens } from '../types/navigators'
 
 const ImportWallet: React.FC = () => {
-  const navigation = useNavigation<StackNavigationProp<OnboardingStackParams, Screens.ImportWallet>>()
+  
 
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Import your wallet here</Text>
-      <Button
-        title="Continue"
-        onPress={() => navigation.navigate(Screens.CreatePIN, { flow: 'import' })}
-      />
+      
     </View>
   )
 }
